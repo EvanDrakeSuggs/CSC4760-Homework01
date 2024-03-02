ae95835c0fd4cc1d1d415d16b7c6db6af34dba89
Eddie Gannod <epgannod@gmail.com>
2024-03-02 14:23:12 -0600

    1st commit for problem 2

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -9,8 +9,13 @@ int main(int argc, char* argv[]) {
   // set n and m, you can change these values
   int n,m = 16;
   // Make View
-  
+  Kokkos::View<int**> prob_2("prob_2", n, m);
   // set values to 1000 * i * j;
+  for (int i=0; i < n; i++){
+    for (int j=0; j < m; j++){
+      prob_2(i,j) = 1000 * i * j;
+    }
+  }
   }
   Kokkos::finalize();
 }
